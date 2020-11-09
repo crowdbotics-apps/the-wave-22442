c96c81d43132c56f17cabf42b6a68fd0af9a6d05
Crowdbotics <team@crowdbotics.com>
2020-11-09 16:38:04 +0000

    Update models

--- backend/the_wave_22442/wsgi.py
+++ backend/the_wave_22442/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_wave_22442.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_wave_22442.settings")
 
 application = get_wsgi_application()
